14905d413200d65b99130e9f38d2a9cba8c70d69
Jeremy Howard <github@jhoward.fastmail.fm>
2026-01-31 20:00:48 +1000

    bump

--- ipymini/__init__.py
+++ ipymini/__init__.py
@@ -1,7 +1,8 @@
+__version__ = "0.1.5"
+
 from importlib.metadata import PackageNotFoundError, version
 from .kernel import run_kernel
 
-__version__ = "0.1.4"
 try: __version__ = version("ipymini")
 except PackageNotFoundError: pass
 
